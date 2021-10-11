098fce82f05c34b62db01edbc7742bb7a4080532
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-10-11 22:56:28 +0530

    Map showing successfully

--- src/Components/map/map.js
+++ src/Components/map/map.js
@@ -1,23 +1,27 @@
-import React, {useEffect, useState} from 'react'
-import styles from "./map.module.css"
-import L from 'leaflet'
+import React, { useEffect, useState } from 'react';
+import styles from './map.module.css';
+import L from 'leaflet';
 function Map() {
-    const [mapOptions, setMapOptions] = useState({});
-    useEffect(() => {
-        let obj = {
-            center: [51.958, 9.141],
-            zoom: 10
-        };
-        setMapOptions(obj);
-        let map = new L.map('map', mapOptions);
-        let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-        map.addLayer(layer);
-    }, []); // empty dependency
-    return (
-        <div className ={styles.map} id="map" >
-            
-        </div>
-    )
+  const [mapOptions, setMapOptions] = useState({});
+  useEffect(() => {
+    let obj = {
+      center: [51.958, 9.141],
+      zoom: 10,
+    };
+    setMapOptions(obj);
+    var map = L.map('map').setView([29, 76], 5);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+  }, []); // empty dependency
+  return (
+    <>
+      <div className={styles.mapcontainer}>
+        <div className={[styles.map, 'map'].join(' ')} id='map'></div>
+      </div>
+    </>
+  );
 }
 
-export default Map
+export default Map;
