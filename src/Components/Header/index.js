b675460767289dc81e32659ad5ef61611ea8243f
Aman Raj <amanpie9@gmail.com>
2021-10-12 22:46:20 +0530

    did the logic to get the weather conditons

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,17 @@
 import logo from "../../mlh-prep.png";
+import { useState } from "react";
 
-const Header = ({ city, onChangeCity }) => {
+const Header = ({ city, onChangeCity, results, isLoaded }) => {
+  const [image, setImage] = useState(logo);
   return (
     <div>
-      <img className="logo" src={logo} alt="MLH Prep Logo"></img>
+      <img className="logo" src={image} alt="MLH Prep Logo"></img>
+      {isLoaded && results && (
+        <h1>
+          {results.weather[0].main}
+          {console.log(results)}
+        </h1>
+      )}
       <h2>Enter a city below 👇</h2>
       <input
         type="text"
