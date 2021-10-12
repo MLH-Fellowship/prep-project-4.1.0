06b9aa1fd7e25fa27196469b9822ff54b33603bb
thedevyansh <schawla333333@gmail.com>
2021-10-12 23:07:36 +0530

    Replace single quotes with double quotes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Header from './Components/Header';
-import Card from './Components/Card';
-import Loader from 'react-loader-spinner';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Card from "./Components/Card";
+import Loader from "react-loader-spinner";
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState("");
   const [results, setResults] = useState(null);
 
   useEffect(() => {
@@ -48,18 +48,18 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if (city !== '') {
+    if (city !== "") {
       fetch(
-        'https://api.openweathermap.org/data/2.5/weather?q=' +
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
           city +
-          '&units=metric' +
-          '&appid=' +
+          "&units=metric" +
+          "&appid=" +
           process.env.REACT_APP_APIKEY
       )
         .then((res) => res.json())
         .then(
           (result) => {
-            if (result['cod'] !== 200) {
+            if (result["cod"] !== 200) {
               setIsLoaded(false);
             } else {
               setIsLoaded(true);
