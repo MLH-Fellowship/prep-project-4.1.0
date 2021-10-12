ff59c729c6ea8c53a64e1bbdf368fa87dfbe35e6
thedevyansh <schawla333333@gmail.com>
2021-10-12 18:06:53 +0530

    Show current position using Geolocation API 🌎

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 ReactDOM.render(
   <React.StrictMode>
