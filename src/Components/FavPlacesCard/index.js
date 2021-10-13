88f81ae1c050d45d87c3aa2045a93908ab4745b2
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-10-13 13:41:26 +0530

    Data fetching work complete

--- src/Components/FavPlacesCard/index.js
+++ src/Components/FavPlacesCard/index.js
@@ -1,10 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './style.css';
 function Index(props) {
   const { place } = props;
   const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
   const API_KEY = process.env.REACT_APP_APIKEY;
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const [wind, setWind] = useState('');
+  const [humidity, setHumidity] = useState('');
+  const [icon, setIcon] = useState('');
+  const [description, setDescription] = useState('');
+  const [temperature, setTemperature] = useState('');
+  const [weekly, setWeekly] = useState([]);
+  const [minTemp, setminTemp] = useState([]);
+  const [maxTemp, setmaxTemp] = useState([]);
   useEffect(() => {
     async function getDetails() {
       const { data } = await axios.get(
@@ -17,9 +26,42 @@ function Index(props) {
         `${BASE_URL}onecall?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
       );
       oneApiData = oneApiData.data;
-      console.log(oneApiData);
-      const wind = oneApiData?.current?.wind_speed;
-      const humidity = oneApiData?.current?.humidity;
+      const { current } = oneApiData;
+
+      setWind(current.wind_speed);
+      setHumidity(current.humidity);
+      setIcon(current.weather[0].icon);
+      setDescription(current.weather[0].description);
+
+      let temp = current.temp;
+      temp = (5 / 9) * temp - 32;
+
+      setTemperature(temp);
+
+      const requiredDays = [];
+      const date = new Date().getDay();
+      for (let i = date; i < date + 5; i++) requiredDays.push((i + 1) % 7);
+
+      setWeekly(
+        requiredDays.map((idx) => {
+          return days[idx];
+        })
+      );
+      const daily = oneApiData.daily;
+      const filteredDailyData = daily.filter((ele, idx) => {
+        return idx < 5;
+      });
+      setminTemp(
+        filteredDailyData.map((dailyObject, idx) => {
+          return dailyObject.temp.min;
+        })
+      );
+
+      setmaxTemp(
+        filteredDailyData.map((dailyObject, idx) => {
+          return dailyObject.temp.max;
+        })
+      );
     }
     getDetails();
   }, []);
