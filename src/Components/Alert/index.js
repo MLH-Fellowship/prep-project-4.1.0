bffac696ab2ee3f847b790975cf9ddb8dfa16bcd
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-10-19 13:29:17 +0530

    cards successfully formed

--- src/Components/Alert/index.js
+++ src/Components/Alert/index.js
@@ -36,10 +36,7 @@ function Index() {
         {/* swiper-slide swiper-slide-duplicate swiper-slide-next swiper-slide-duplicate-prev */}
         <SwiperSlide>
           <div className="blog-slider__img">
-            <img
-              src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759872/kuldar-kalvik-799168-unsplash.jpg"
-              alt=""
-            />
+            <img src="images/warning_without_bg.png" alt="" />
           </div>
           <div className="blog-slider__content">
             <span className="blog-slider__code">30 December 2019</span>
