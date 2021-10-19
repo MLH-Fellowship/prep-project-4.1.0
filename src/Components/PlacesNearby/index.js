2ee22ff32a6faeb1e85ab750c0ba49a82f2bb360
Akhil Bhalerao <iamakhil1408@gmail.com>
2021-10-19 18:34:32 +0530

    errors resolved for css and env

--- src/Components/PlacesNearby/index.js
+++ src/Components/PlacesNearby/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import './placesNearby.css'
+import "./placesNearby.css";
 
 export default class PlacesNearby extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       placesNearby: [],
-      loading: true
+      loading: true,
     };
   }
 
@@ -17,13 +17,13 @@ export default class PlacesNearby extends React.Component {
   getPlacesNearby() {
     this.setState({ loading: true });
     const { lat, lng } = this.props;
-    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=500&type=restaurant&key=${REACT_APP_GMAPSAPI}`;
+    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=500&type=restaurant&key=${process.env.REACT_APP_GMAPSAPI}`;
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         this.setState({
           placesNearby: data.results,
-          loading: false
+          loading: false,
         });
       });
   }
@@ -36,7 +36,7 @@ export default class PlacesNearby extends React.Component {
           <div className="loading">Loading...</div>
         ) : (
           <ul>
-            {placesNearby.map(place => (
+            {placesNearby.map((place) => (
               <li key={place.id}>
                 <h3>{place.name}</h3>
                 <p>{place.vicinity}</p>
