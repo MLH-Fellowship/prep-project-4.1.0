f88160105c9f6add6c80d8cae7c392554094056f
Hugo <hugointe@gmail.com>
2021-10-19 00:35:27 -0300

    Preliminary frontend and backend

--- src/Components/PlacesNearby/index.js
+++ src/Components/PlacesNearby/index.js
@@ -1,7 +1,50 @@
 import React from "react";
+import './placesNearby.css'
 
-function PlacesNearby() {
-  return <div></div>;
+export default class PlacesNearby extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      placesNearby: [],
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    this.getPlacesNearby();
+  }
+
+  getPlacesNearby() {
+    this.setState({ loading: true });
+    const { lat, lng } = this.props;
+    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=500&type=restaurant&key=${REACT_APP_GMAPSAPI}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          placesNearby: data.results,
+          loading: false
+        });
+      });
+  }
+
+  render() {
+    const { placesNearby, loading } = this.state;
+    return (
+      <div className="places-nearby">
+        {loading ? (
+          <div className="loading">Loading...</div>
+        ) : (
+          <ul>
+            {placesNearby.map(place => (
+              <li key={place.id}>
+                <h3>{place.name}</h3>
+                <p>{place.vicinity}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  }
 }
-
-export default PlacesNearby;
