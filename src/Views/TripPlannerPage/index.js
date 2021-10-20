324ab15b33b9e430bf8b39477b86d952f38f040c
Manasvi070902 <manasvi.alimchandani2019@vitstudent.ac.in>
2021-10-20 20:41:22 +0530

    header modified

--- src/Views/TripPlannerPage/index.js
+++ src/Views/TripPlannerPage/index.js
@@ -1,13 +1,27 @@
 import TripHeader from "./TripHeader"
 import TripNavBar from "./TripNavbar"
-
+import WeatherInfo from "../../Components/WeatherInfo";
+import useWeather from "../../customHooks/useWeather";
 const TripPlannerPage = (props) => {
+    // const {
+    //     city,
+    //     results,
+    //     isLoaded,
+    //     setCity,
+    //     setResults,
+    //     places,
+    //     setPlaces,
+    //     error,
+    //   } = useWeather();
     return(
 <>
 
 <TripHeader >
 <TripNavBar />
 </TripHeader>
+{/* <WeatherInfo data={results} /> */}
+
+
 </>
     )
 }
