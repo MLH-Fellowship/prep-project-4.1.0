ec4ef351b33a9ca19847b8a63042978eefc2cbc5
Rudransh Sharma <67827010+rudranshsharma123@users.noreply.github.com>
2021-10-20 19:21:17 +0530

    Cleaned code

--- src/Components/WeatherInfo/index.js
+++ src/Components/WeatherInfo/index.js
@@ -12,12 +12,10 @@ import "./weatherinfo.css";
 const getTime = (timestamp) => {
   var date = new Date(timestamp * 1000);
   var hours = date.getHours();
-  // Minutes part from the timestamp
   var minutes = "0" + date.getMinutes();
-  // Seconds part from the timestamp
   var seconds = "0" + date.getSeconds();
 
-  // Will display time in 10:30:23 format
+
   var formattedTime =
     hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
 
