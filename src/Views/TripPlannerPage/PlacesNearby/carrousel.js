0f4a046593a1c0dd6df097bfc25d40c7cd7c660b
Hugo <hugointe@gmail.com>
2021-10-22 00:16:57 -0300

    Card carrousel update

---
+++ src/Views/TripPlannerPage/PlacesNearby/carrousel.js
@@ -0,0 +1,56 @@
+import { React, Component } from "react";
+import ReactCardCarousel from "react-card-carousel";
+import './placesNearby.css'
+
+export default class Carrousel extends Component {
+
+  
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        items: []
+      }
+      this.props.places.map((place) => {
+          var name = place.name;
+          var address = place.vicinity;
+          var rating = place.rating;
+          var item = {name, address, rating};
+          this.state.items.push(item);
+      });
+  }
+
+  componentDidMount() {
+    console.log(this.state.items);
+  }
+
+  static get CARD_STYLE() {
+    return {
+      height: "200px",
+      width: "200px",
+      paddingTop: "80px",
+      textAlign: "center",
+      background: "#1C539F",
+      color: "#FFF",
+      fontSize: "12px",
+      textTransform: "uppercase",
+      borderRadius: "10px",
+    };
+  }
+
+  render() {
+    return (
+      <div className="carrousel">
+        <ReactCardCarousel autoplay={true} autoplay_speed={3500}>
+        {this.state.items.map((item, index) => (
+          <div key={index} style={Carrousel.CARD_STYLE}>
+            <p className="name">{item.name}</p>
+            <p className="address">Address: {item.vicinity}</p>
+            <p className="rating">Ratings: {item.rating}</p>
+          </div>
+        ))}
+      </ReactCardCarousel>
+      </div>
+    );
+  }
+}
---
+++ src/Views/TripPlannerPage/PlacesNearby/index.js
@@ -0,0 +1,109 @@
+import React from "react";
+import "./placesNearby.css";
+import axios from "axios";
+import Carrousel from "./carrousel";
+
+export default class PlacesNearby extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      restaurantsNearby: [],
+      lodgingNearby: [],
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getRestaurantsNearby();
+    this.getLodgingNearby();
+  }
+
+  // reference Link : https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe
+  // https://cors-anywhere.herokuapp.com/corsdemo
+
+  getRestaurantsNearby() {
+    this.setState({ loading: true });
+    // const { lat, lng } = this.props;
+    if (this.props.results != null || this.props.results != undefined) {
+      console.log("Results");
+      console.log(this.props.results);
+      const lat = this.props.results[1].coord.lat;
+      const lng = this.props.results[1].coord.lon;
+
+      const url = `https://cryptic-oasis-82460.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=5000&type=restaurant&key=${process.env.REACT_APP_GMAPSAPI}`;
+      fetch(url, {
+        method: "GET",
+        headers: {},
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          var formated_data_RESTURANTS = [];
+          for (var i = 0; i < 3; i++) {
+            formated_data_RESTURANTS.push({
+              name: data["results"][i]["name"],
+              vicinity: data["results"][i]["vicinity"],
+              rating: data["results"][i]["rating"],
+            });
+          }
+          console.log(formated_data_RESTURANTS);
+          this.setState({
+            restaurantsNearby: formated_data_RESTURANTS,
+            loading: false,
+          });
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+
+  getLodgingNearby() {
+    this.setState({ loading: true });
+    // const { lat, lng } = this.props;
+    if (this.props.results != null || this.props.results != undefined) {
+      console.log("Results");
+      console.log(this.props.results);
+      const lat = this.props.results[1].coord.lat;
+      const lng = this.props.results[1].coord.lon;
+
+      const url = `https://cryptic-oasis-82460.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=5000&type=lodging&key=${process.env.REACT_APP_GMAPSAPI}`;
+      fetch(url, {
+        method: "GET",
+        headers: {},
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          var formated_data_LODGES = [];
+          for (var i = 0; i < 3; i++) {
+            formated_data_LODGES.push({
+              name: data["results"][i]["name"],
+              vicinity: data["results"][i]["vicinity"],
+              rating: data["results"][i]["rating"],
+            });
+          }
+          console.log(formated_data_LODGES);
+          this.setState({
+            lodgingNearby: formated_data_LODGES,
+            loading: false,
+          });
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+
+  render() {
+    // if (!this.state.loading) {
+    //   console.log("RESTURANTS RENDER: ", this.state.restaurantsNearby);
+    //   console.log("LODGES RENDER: ", this.state.lodgingNearby);
+    // }
+    return (
+      <div className="places-nearby">
+        {/* {!loading && <Carrousel places={restaurantsNearby} loading={loading} />}
+        {!loading && <Carrousel places={lodgingNearby} loading={loading} />} */}
+      </div>
+    );
+  }
+}
