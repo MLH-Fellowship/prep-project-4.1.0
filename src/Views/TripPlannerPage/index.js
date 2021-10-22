262fe8a337722876d2be86df295816eee7b2da82
Manasvi070902 <manasvi.alimchandani2019@vitstudent.ac.in>
2021-10-22 11:19:30 +0530

    issue solved

--- src/Views/TripPlannerPage/index.js
+++ src/Views/TripPlannerPage/index.js
@@ -10,7 +10,11 @@ const TripPlannerPage = (props) => {
   const [results, setResults] = useState([]);
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-
+  function getPara(){
+    console.log('results is ',results);
+    console.log('results length is',results.length);
+    console.log('results',results[0])
+  }
   useEffect(() => {
     cities.map((obj) => {
       console.log(obj);
@@ -29,11 +33,15 @@ const TripPlannerPage = (props) => {
               setIsLoaded(false);
             } else {
               setIsLoaded(true);
-              if (obj.type == "from") {
-                results.splice(0, 0, result);
+              if (obj.type === "from") {
+                // results.splice(0, 0, result);
+                console.log('result before pushing 0',result);
+                setResults([...results,result]);
                
               } else {
-                results.splice(1, 0, result);
+                // results.splice(1, 0, result);
+                console.log('result before pushing 1',result);
+                setResults([...results,result]);
               }
             }
           },
@@ -41,17 +49,22 @@ const TripPlannerPage = (props) => {
             setIsLoaded(true);
             setError(error);
           }
-        );
+        ).catch((e)=>{
+          alert(e);
+        });
     });
   }, [cities]);
 
   return (
     <>
       <CityContext.Provider value={[results, setResults, cities, setCities]}>
+      {getPara()}
         <TripHeader>
           <TripNavBar />
         </TripHeader>
+       {results.length > 0 && <RequiredThings results={results[0]} />}
       </CityContext.Provider>
+   
 
       {/* <PlacesNearby city={city} setCity={onChangeCity} /> */}
     </>
